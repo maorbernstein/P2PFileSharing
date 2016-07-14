6d2796ded7eb69f99c80e5fd5fc14e1f40f85590
vduongscu <vduong@scu.edu>
2016-07-13 17:24:41 -0700

    Add Network Manager Interface

---
+++ src/NetworkManager_IF.java
@@ -0,0 +1,8 @@
+
+public interface NetworkManager_IF {
+	public void sendAddFile(String filename);
+	public void sendAddUser(String username, String ip);
+	public void sendModifyFile(String filename);
+	public void sendGetFile(String filename);
+	public void sendExit();
+}
