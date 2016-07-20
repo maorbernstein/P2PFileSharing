f82ffde4c0a7cd606d172e8b132fa61dde14c2e8
davidlywei <davidlywei@gmail.com>
2016-07-20 14:54:42 -0700

    Initial commit of interface between GUI and FileManager

--- src/GUIFileManager_IF.java
+++ src/GUIFileManager_IF.java
@@ -1,1 +1,8 @@
-// Temp file
+public interface GUIFileManager_IF
+{
+    public void fileOpSuccess();
+    public void fileNotFount();
+    public void IOexception();
+    public void percentComplete(double percent);
+    public String getDownloadDirectory();
+}
