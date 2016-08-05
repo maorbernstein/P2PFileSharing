389c8d8f457b21187a2c5b3baf28c7a3ffc3831f
davidlywei <davidlywei@gmail.com>
2016-08-05 02:55:24 -0700

    Added Interfaces, removed code into other classes

--- src/P2PFSGui.java
+++ src/P2PFSGui.java
@@ -1,20 +1,16 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,7 +18,7 @@ import java.util.regex.Pattern;
  * <p>
  * Class Name:     P2PFSGUI
  * <p>
- * Purpose:        PURPOSE
+ * Purpose:        The Coordinator class for the GUI
  * <p>
  * Create By:      David Wei
  * Date:           7/18/2016
@@ -31,86 +27,91 @@ import java.util.regex.Pattern;
  * <p>
  ****************************************************************************************/
 
-public class P2PFSGui extends Application
+public class P2PFSGui extends Application implements
+        GUIFileManager_IF, GUINetworkCoordinator_IF, GUIUserManager_IF
 {
+    // Define the GUI Elements
     private GridPane startUpGrid;
     private Scene startUpScene;
     private Stage startUpStage;
 
-    private VBox mainVB;
-    private Scene mainScene;
-    private Stage mainStage;
-    private TableView mainTableView;
-
-    private ScrollBar scrollBar;
-
+    // Username of offline person
     private String username;
 
-    private final int MAX_WINDOW_HEIGHT = 500;
+    // Define guiElems object to hold all GUI components
+    private P2PFSGui_elements guiElems;
+
+    // Define error element to throw notifications as needed
+    private P2PFSGui_notification notify = new P2PFSGui_notification();
+
+    // Define regular expression for format of username
     private final String USERNAME_REGEX = "^[A-Z|a-z][A-Z|a-z|0-9]+$";
 
-    private Image newFile;
-    private Image downloadingFile;
-    private Image normalFile;
-    private Image updatedFile;
+    // Define Username max and min lengths
+    private final int USERNAME_MAX_LENGTH = 10;
+    private final int USERNAME_MIN_LENGTH = 3;
 
+    // Start the GUI
     @Override
     public void start(Stage mainStage)
     {
-        newFile = new Image("/resources/newFile.png");
-        normalFile = new Image("/resources/normalFile.png");
-        downloadingFile = new Image("/resources/downloadingFile.png");
-        updatedFile = new Image("/resources/updatedFile.png");
-
-        //newFile = new Image(getClass().getResourceAsStream("newFile.png");
-        //normalFile = new Image(getClass().getResourceAsStream("normalFile.png"));
-        //downloadingFile = new Image(getClass().getResourceAsStream("downloadingFile.png"));
-        //updatedFile = new Image(getClass().getResourceAsStream("updatedFile.png"));
+        // Send GUI Stage element to guiElems
+        guiElems = new P2PFSGui_elements(mainStage);
 
         createStartUpScene();
     }
 
+    // Create popup window to ask for username and whether to join network or not
     private void createStartUpScene()
     {
+        // Create buttons for join and create network
         Button joinNetworkBtn = new Button();
         joinNetworkBtn.setText("Join Network");
 
         Button createNetworkBtn = new Button();
         createNetworkBtn.setText("Create Network");
-        //HBox createNetworkBHB = new HBox();
-        //createNetworkBHB.setAlignment(Pos.CENTER_RIGHT);
-        //createNetworkBHB.getChildren().add(createNetworkBtn);
 
+        // Create text and text field to grab username
         Label usernameLabel = new Label("Username: ");
         TextField usernameTextField = new TextField();
 
+        // Add event handler to join network button
         joinNetworkBtn.setOnAction( (ActionEvent e) -> {
 
+            // Grab Username
             username = usernameTextField.getText();
             System.out.println("Joining Network");
 
+            // Grab invitation file
             FileChooser invFileChooser = new FileChooser();
             invFileChooser.setTitle("Select Invitation File");
             File file = invFileChooser.showOpenDialog(startUpStage);
-
             System.out.println("Opening file: " + file.getName());
 
-            createMainStage();
-
-            if(username.length() < 3)
+            // Check for valid username
+            if(checkUsername())
             {
-                createErrorStage("Username must be longer than 3 characters.");
-                System.out.println("Username: " + username + " is invalid.");
-            }
-            else
-            {
-                // TODO: Add try catch block to verify joining network went OK
-
-                createMainStage();
-
+                System.out.println("Username verified");
                 System.out.println("Username: " + username);
-                mainStage.show();
-                startUpStage.close();
+
+                if(true) // TODO: add join network code here
+                {
+                    System.out.println("Network joined successfully");
+
+                    createMainStage();
+
+                    P2PFSGui_user me = new P2PFSGui_user(username, guiElems, true);
+                    guiElems.setMe(me);
+                    guiElems.redraw();
+
+                    guiElems.getStage().show();
+                    startUpStage.close();
+                }
+                else
+                {
+                    System.out.println("Failed to join network");
+                    notify.createError("Failed to join network");
+                }
             }
         });
 
@@ -119,141 +120,106 @@ public class P2PFSGui extends Application
             username = usernameTextField.getText();
             System.out.println("Creating Network");
 
+            // Verify Username
             if(checkUsername())
             {
+                System.out.println("Username verified");
+                System.out.println("Username: " + username);
+
                 createMainStage();
 
-                System.out.println("Username: " + username);
-                mainStage.show();
+                // Set current user as main user.
+                P2PFSGui_user me = new P2PFSGui_user(username, guiElems, true);
+                guiElems.setMe(me);
+                guiElems.redraw();
+
+                guiElems.getStage().show();
                 startUpStage.close();
             }
         });
 
+        // Setup Grid to hold elements for Startup Pop-Up
         startUpGrid = new GridPane();
         startUpGrid.setAlignment(Pos.CENTER);
         startUpGrid.setHgap(10);
         startUpGrid.setVgap(10);
-        //startUpGrid.setPadding(new Insets(25, 25, 25, 25));
 
+        // Add created elements to the GridPane
         startUpGrid.add(usernameLabel, 0, 0);
         startUpGrid.add(usernameTextField, 1, 0);
         startUpGrid.add(joinNetworkBtn, 0, 1);
-        //startUpGrid.add(createNetworkBHB, 1, 1);
         startUpGrid.add(createNetworkBtn, 1, 1);
 
-        startUpScene = new Scene(startUpGrid, 300, 100);
+        // Set the GridPane as element in a new Scene
+        startUpScene = new Scene(startUpGrid, guiElems.POPUP_WINDOW_WIDTH,
+                guiElems.POPUP_WINDOW_HEIGHT);
 
+        // Create new Stage and add Scene created earlier
         startUpStage = new Stage();
         startUpStage.setScene(startUpScene);
+        // Set Pop-up as temporary
         startUpStage.initModality(Modality.APPLICATION_MODAL);
         startUpStage.setTitle("P2P File Sharing");
 
+        // Show the startup pop-up
         startUpStage.show();
     }
 
     private void createMainStage()
     {
+        // Create Main VBox to contain all main window elements
         VBox mainGroup = new VBox();
-        mainScene = new Scene(mainGroup, 510, MAX_WINDOW_HEIGHT);
+        guiElems.setScene(new Scene(mainGroup, guiElems.MAX_WINDOW_WIDTH,
+                guiElems.MAX_WINDOW_HEIGHT));
 
-        final Menu menu = new Menu("Invite");
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menu);
+        //
+        final Button inviteBtn = new Button("Invite");
+        inviteBtn.setOnAction( (ActionEvent e) -> {
+            // TODO: Add generate Invitation file
+            // TODO: Print out Invitation file Location
 
-        mainVB = new VBox();
-        mainVB.setLayoutX(5);
-        mainVB.setSpacing(10);
-        mainVB.setMinHeight(MAX_WINDOW_HEIGHT - 30);
-        mainVB.setStyle("-fx-padding: 20;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;");
-
-        populateVBox(mainVB);
-
-        final ScrollPane scrollPane = new ScrollPane(mainVB);
-        scrollPane.setMinHeight(mainVB.getMinHeight());
-
-        mainGroup.getChildren().addAll(menuBar, mainVB, scrollPane);
-        //mainGroup.getChildren().addAll(menuBar, mainVB);
-        mainStage = new Stage();
-        mainStage.setScene(mainScene);
-        mainStage.setTitle("P2P File Sharing");
-    }
-
-    private void createErrorStage(String errorText)
-    {
-        GridPane errorGrid = new GridPane();
-        errorGrid.setAlignment(Pos.CENTER);
-        errorGrid.setHgap(10);
-        errorGrid.setVgap(10);
-
-        Label text = new Label(errorText);
-
-        Button okBtn = new Button();
-        okBtn.setText("Ok");
-        HBox okBtnFormat = new HBox();
-        okBtnFormat.setAlignment(Pos.CENTER);
-        okBtnFormat.getChildren().add(okBtn);
-
-        errorGrid.add(text,0, 0);
-        errorGrid.add(okBtnFormat, 0, 1);
-
-        Scene errorScene = new Scene(errorGrid, 300, 100);
-
-        Stage errorStage = new Stage();
-        errorStage.setScene(errorScene);
-        errorStage.setTitle("Error");
-
-        errorStage.show();
-
-        okBtn.setOnAction((ActionEvent e) -> {
-
-            errorStage.close();
+            notify.createNotification("Invitation file Generated at \nDIRECTORY", "Invitation File Generated");
+            System.out.println("Generated Invitation File");
         });
 
-    }
+        // Create ScrollPane to contain all elements and add scroll bar
+        guiElems.setScrollPane(new ScrollPane(new TabPane()));
+        guiElems.getScrollPane().setMinHeight(guiElems.MAX_WINDOW_HEIGHT - 30);
 
-    private void populateVBox(VBox vb)
-    {
-        addUser(vb, username);
-
-        // Add code to add files and other users
-        addFile(vb, username, "normal", normalFile);
-
-        addUser(vb, "Test User");
-
-        // Add code to add files and other users
-        addFile(vb, "Test User", "downloading", downloadingFile);
-        addFile(vb, "Test User", "normal", normalFile);
-        addFile(vb, "Test User", "updating", updatedFile);
-        addFile(vb, "Test User", "new1", newFile);
-        addFile(vb, "Test User", "new2", newFile);
-        addFile(vb, "Test User", "new3", newFile);
-        addFile(vb, "Test User", "new4", newFile);
-        addFile(vb, "Test User", "new5", newFile);
-        addFile(vb, "Test User", "new6", newFile);
-        addFile(vb, "Test User", "new7", newFile);
-        addFile(vb, "Test User", "new8", newFile);
-        addFile(vb, "Test User", "new9", newFile);
-        addFile(vb, "Test User", "new10", newFile);
+        // Add all elements to main group and set the stage
+        mainGroup.getChildren().addAll(inviteBtn, guiElems.getScrollPane());
+        guiElems.setStage(new Stage());
+        guiElems.getStage().setScene(guiElems.getScene());
+        guiElems.getStage().setTitle("P2P File Sharing");
     }
 
     private Boolean checkUsername()
     {
+        // Used for Regular Expression Pattern Matching
         Pattern p = Pattern.compile(USERNAME_REGEX);
         Matcher m = p.matcher(username);
 
-        if(username.length() < 3)
+        // Checks if Username is more than 3 characters
+        if(username.length() < USERNAME_MIN_LENGTH)
         {
-            createErrorStage("Username must be longer than 3 characters.");
+            notify.createError("Username must be longer than " + USERNAME_MIN_LENGTH
+                    + " characters.");
             System.out.println("Username: " + username + " is invalid.");
 
             return false;
         }
+        else if(username.length() > USERNAME_MAX_LENGTH)
+        {
+            notify.createError("Username must be less than " + USERNAME_MAX_LENGTH
+                    + " characters long.");
+            System.out.println("Username: " + username + " is invalid.");
+
+            return false;
+        }
+        // Checks that Username is of pattern (Alpha char) + (Alpha numeric char)*x
         else if (!m.find())
         {
-            createErrorStage("Usernames may not contain the following characters:\n!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+            notify.createError("Usernames may not contain the following characters:\n!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
             System.out.println("Username: " + username + " is invalid.");
 
             return false;
@@ -262,72 +228,171 @@ public class P2PFSGui extends Application
         return true;
     }
 
-    private void addUser(VBox vb, String username)
+    @Override
+    public void fileOpSuccess()
     {
-        HBox hb = new HBox();
-
-        Label user = new Label(username);
-        user.setFont(new Font("Arial", 15));
-        user.setMinWidth(400);
-
-        Button addBtn = new Button("Add");
-
-        hb.getChildren().addAll(user, addBtn);
-
-        Separator sepTop = new Separator();
-        sepTop.setOrientation(Orientation.HORIZONTAL);
-        Separator sepBottom = new Separator();
-        sepBottom.setOrientation(Orientation.HORIZONTAL);
-
-        vb.getChildren().add(sepBottom);
-        vb.getChildren().add(hb);
-        vb.getChildren().add(sepTop);
+        System.out.println("File Operation was successful");
     }
 
-    private void addFile(VBox vb, String owner, String filename, Image fileImg)
+    @Override
+    public void fileNotFound()
     {
-        GridPane gp = new GridPane();
+        notify.createError("File not found");
+        System.out.println("File not Found");
+    }
 
-        ImageView img = new ImageView(fileImg);
-        img.setFitHeight(10);
-        img.setFitWidth(10);
-        img.setPreserveRatio(true);
+    @Override
+    public void IOexception()
+    {
+        notify.createError("I/O Exception");
+        System.out.println("I/O Exception encountered");
+    }
 
-        Label icon = new Label("", img);
-        Label file = new Label(filename);
-
-        icon.setMinWidth(50);
-        file.setMinWidth(275);
-
-        Button updateBtn = new Button("Update");
-        Button removeBtn = new Button("Remove");
-        Button getBtn = new Button("Get");
-
-        ProgressBar progressBar = new ProgressBar();
-
-        progressBar.setProgress(0.75);
-
-        gp.add(icon, 0, 0);
-        gp.add(file, 1, 0);
-
-        if(owner == username)
+    @Override
+    public void uploadPercentComplete(String user, String filename, double percent)
+    {
+        // Update percentage of file
+        try
         {
-            gp.add(updateBtn, 2, 0);
-            gp.add(removeBtn, 3, 0);
-        }
-        else
+            guiElems.getUploadUser(user).getFile(filename).setUploadProgress(percent);
+        } catch (NoSuchElementException e)
         {
-            if(fileImg == downloadingFile)
-            {
-                gp.add(progressBar, 3, 0);
-            }
-            else if(fileImg != normalFile)
-            {
-                gp.add(getBtn, 3, 0);
-            }
+            System.out.println("Update Percentage file not found. \n    Username: " + user
+                    + "\n   Filename: " + filename);
         }
 
-        vb.getChildren().add(gp);
+        if(percent == 1)
+        {
+            guiElems.finishUpload(user, filename);
+        }
+
+        System.out.println("Updated Percentage");
+    }
+
+    @Override
+    public void downloadPercentComplete(String user, String filename, double percent)
+    {
+        // Update percentage of file
+        try
+        {
+            guiElems.getUser(user).getFile(filename).setDownloadProgress(percent);
+        } catch (NoSuchElementException e)
+        {
+            System.out.println("Update Percentage file not found. \n    Username: " + user
+                    + "\n   Filename: " + filename);
+        }
+
+        if(percent == 1)
+        {
+            guiElems.finishDownload(user, filename);
+        }
+
+        System.out.println("Updated Percentage");
+    }
+
+    @Override
+    public void addNewFile(String[][] files)
+    {
+        for(int i = 0; i < files.length; i++)
+        {
+            for (int j = 1; j < files[i].length; j++)
+            {
+                // TODO: fix the file name / username
+                guiElems.getUser(files[i][0]).addFile(
+                        new P2PFSGui_file(files[i][j], guiElems, false));
+
+                System.out.println("Added User: " + files[i][0] + " Filename: " + files[i][j]);
+            }
+        }
+
+        guiElems.redraw();
+    }
+
+    @Override
+    public void removeFile(String[][] files)
+    {
+        for(int i = 0; i < files.length; i++)
+        {
+            for (int j = 1; j < files[i].length; j++)
+            {
+                try
+                {
+                    // TODO: fix the file name / username
+                    guiElems.getUser(files[i][0]).removeFile(files[i][j]);
+                    System.out.println("Removed User: " + files[i][0] + " Filename: " + files[i][j]);
+                } catch (NoSuchElementException e)
+                {
+                    System.out.println("Remove Element not found. \n    Username: " + files[i][0]
+                            + "\n   Filename: " + files[i][j]);
+                }
+            }
+        }
+
+        guiElems.redraw();
+    }
+
+    @Override
+    public void updateFile(String[][] files)
+    {
+        for(int i = 0; i < files.length; i++)
+        {
+            for (int j = 1; j < files[i].length; j++)
+            {
+                // TODO: fix the file name / username
+                try
+                {
+                    guiElems.getUser(files[i][0]).getFile(
+                            files[i][j]).setStatus(P2PFSGui_file.FileStatus.updatedFile);
+                }catch (NoSuchElementException e)
+                {
+                    System.out.println("Update Element not found. \n    Username: " + files[i][0]
+                            + "\n   Filename: " + files[i][j]);
+                }
+                System.out.println("Updated User: " + files[i][0] + " Filename: " + files[i][j]);
+            }
+        }
+
+        guiElems.redraw();
+    }
+
+    @Override
+    public void addUser(String username)
+    {
+        guiElems.addUser(new P2PFSGui_user(username, guiElems, false));
+        System.out.println("Added User: " + username );
+    }
+
+    @Override
+    public void removeUser(String username)
+    {
+        guiElems.removeUser(username);
+        System.out.println("Removed User: " + username );
+    }
+
+    @Override
+    public void connectionStatus(boolean established)
+    {
+        if (!established)
+        {
+            notify.createError("Connection Failure");
+            System.out.println("Connection Failure");
+        } else
+        {
+            System.out.println("Connection Successful");
+
+        }
+    }
+
+    @Override
+    public void uploadStarted(String user, String filename)
+    {
+        guiElems.upload(user, filename);
+        System.out.println("Upload Started");
+    }
+
+    @Override
+    public void stop()
+    {
+        guiElems.closeGui();
     }
 }
-
